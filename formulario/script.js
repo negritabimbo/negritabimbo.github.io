3102e71b772782265af47bfb618b300aef6c3987
negritabimbo <56596292+negritabimbo@users.noreply.github.com>
2024-04-08 19:14:43 -0600

    Add files via upload

---
+++ formulario/script.js
@@ -0,0 +1,13 @@
+function DarkMode() 
+{
+    const body = document.body;
+    const form = document.querySelector('form'); // Select the form element
+    //const button = document.querySelectorAll('button'); //
+    //button.classList.toggle("dark-mode");
+   // .toggle("dark-mode");
+    //classList.toggle("dark-mode");
+    body.classList.toggle("dark-mode");
+    form.classList.toggle("dark-mode");
+
+
+}
