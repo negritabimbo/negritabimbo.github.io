aa46bc66754cc087083e54da3188be4ec3b1f74a
negritabimbo <56596292+negritabimbo@users.noreply.github.com>
2024-04-10 20:18:17 -0600

    Add files via upload

---
+++ prac/script.js
@@ -0,0 +1,13 @@
+const urlTraducciones = 'idiomas.json';
+
+// Función para cambiar el idioma
+function cambiarIdioma(idioma) {
+    fetch(urlTraducciones)
+        .then(response => response.json())
+        .then(data => {
+            const traduccion = data[idioma];
+            document.getElementById('titulo').textContent = traduccion.titulo;
+            document.getElementById('texto').textContent = traduccion.texto;
+        })
+        .catch(error => console.error('Error al cargar las traducciones:', error));
+}
