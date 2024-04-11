0df962a2e5cf1ed85337d45a3527ceb22097497d
negritabimbo <56596292+negritabimbo@users.noreply.github.com>
2024-04-10 18:26:24 -0600

    Update script.js

--- prac/script.js
+++ prac/script.js
@@ -1,17 +1,44 @@
-import myJson from 'idiomas.json' assert {type: 'json'};
+async function fetchTranslations() {
+    
+    const translations = {
+            "en":{
+                "titulo": "hello",
+                "texto": "thank you very much for visiting this page"
+            },
+            "es":{
+                "titulo":"hola",
+                "texto":"muchas gracias por visitar esta pagina"
+        
+            },
+            "fr":{
+                "titulo":"Salut",
+                "texto":"Merci beaucoup d'avoir visité cette page"
+        
+            },
+            "de":{
+                "titulo":"Hallo",
+                "texto":"Vielen Dank für Ihren Besuch auf dieser Seite"
+            },
+            "it":{
+                "titulo":"ciao",
+                "texto":"grazie mille per aver visitato questa pagina"
+            }
+        };
+    return translations;
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     // Cargar el archivo JSON de idiomas
-    
-    fetch('idiomas.json')
-        .then(response => response.json())
-        .then(data => {
-            // Función para cambiar el idioma
-            function cambiarIdioma(idioma) 
-            {
-                document.getElementById('titulo').innerText = myJson[idioma].titulo;
-                document.getElementById('texto').innerText = myJson[idioma].texto;
-            }
+    function cambiarIdioma(idioma) 
+    {
+        const translations = await fetchTranslations();
+        const translation = translations[idioma];
+        const bigTextElement = document.getElementById("titulo");
+        const paragraphElement = document.getElementById("texto");
+        bigTextElement.textContent = translation.titulo;
+        paragraphElement.textContent = translation.texto;
+    }
+   
             
             // Event listeners para cada botón
             document.getElementById('en').addEventListener('click', function() {
